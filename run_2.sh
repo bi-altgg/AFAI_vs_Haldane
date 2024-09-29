561c28a3e7edd4028edd5c623244667ce377f917
Bishal Ghosh <80319893+bi-altgg@users.noreply.github.com>
2024-09-29 16:27:51 -0500

    Update run_2.sh

--- run_2.sh
+++ run_2.sh
@@ -6,5 +6,7 @@
 #SBATCH     --time=1-12:00:00
 #SBATCH     --mail-type=ALL
 #SBATCH     --mail-user=your.email@utdallas.edu
+#SBATCH     --error=slurm.err                # Error file name
+#SBATCH     --output=slurm.out               # Output file name
 
 prun python WO2_long_time.py
