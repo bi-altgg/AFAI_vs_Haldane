d8310f7bb0403183c61cdc215554d4ec3ba5bc73
Bishal Ghosh <bkg230000@ganymede.utdallas.edu>
2024-10-01 14:22:42 -0500

    Commit before run run2

--- run_2.sh
+++ run_2.sh
@@ -5,8 +5,8 @@
 #SBATCH     --ntasks=32
 #SBATCH     --time=1-12:00:00
 #SBATCH     --mail-type=ALL
-#SBATCH     --mail-user=your.email@utdallas.edu
+#SBATCH     --mail-user=bkg230000@utdallas.edu
 #SBATCH     --error=slurm.err                # Error file name
 #SBATCH     --output=slurm.out               # Output file name
 
-prun python WO2_long_time.py
+prun python3 WO2_long_time.py
